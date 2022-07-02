cef1763ca4a140e9161fc44361971efea7650b17
PedroHercules <pedroherculesdantas@gmail.com>
2022-07-02 00:13:42 -0300

    finalizing component styling

--- src/components/Nft.vue
+++ src/components/Nft.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="card">
-    <img src="../assets/img/image-equilibrium.jpg" alt="">
+    <div id="image-nft">
+      <img src="../assets/img/image-equilibrium.jpg" alt="sss">
+      <div id="hover-nft">
+        <img src="./icons/icon-view.svg" alt="view" />
+      </div>
+    </div>
+    
     <div id="info">
-      <h2>Equilibrium #3429</h2>
+      <h2> <a href="#">Equilibrium &#35;3429</a> </h2>
       <p>Our Equilibrium collection promotes balance and calm.</p>
     </div>
   
     <div id="sub-info">
-      <span>0.041 ETH</span>
-      <span>3 days left</span>
+      <span>
+        <img id="icon-eth" src="./icons/icon-ethereum.svg" alt="eth" /> 
+        0.041 ETH 
+      </span>
+      <span>
+        <img id="icon-clock" src="./icons/icon-clock.svg" alt="clock" />
+        3 days left
+      </span>
     </div>
 
     <div id="created-by">
-      <p>Creation of <span>Jules Wyvern</span></p>
+      <img src="../assets/img/image-avatar.png" alt="avatar" />
+      <span>Creation of <a href="#">Jules Wyvern</a></span>
     </div>
   </div>
 </template>
@@ -33,21 +46,73 @@
     gap: 5px;
     padding: 15px 17px;
     width: 290px;
-    height: 450px;
+    height: 500px;
     background: hsl(216, 50%, 16%);
     border-radius: 1rem;
   }
 
-  #card img {
+  #image-nft {
     width: 100%;
-    height: 220px;
+    height: 250px;
     align-self: center;
     border-radius: .5rem;
+    position: relative;
+    
+  }
+
+  #image-nft img{
+    width: 100%;
+    height: 250px;
+    border-radius: .5rem;
+  }
+
+  #hover-nft {
+    display: none;
+    width: 100%;
+    height: 250px;
+    background: rgba(0, 255, 247, .3);
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    position: absolute;
+    border-radius: .5rem;
+  }
+
+  #hover-nft img{
+    width: 30px;
+    height: 30px;
+  }
+
+  #image-nft img:hover + #hover-nft{
+    display: flex;
+    
+  }
+
+  #image-nft:hover{
+    cursor: pointer;
+  }
+
+  #image-nft img:hover{
+    
+    cursor: pointer;
+  }
+
+  #card img:hover{
+    background: 000;
+    z-index: 9999;
+    cursor: pointer;
   }
   
-  h2{ 
+
+  h2 a{
+    text-decoration: none;
     color: hsl(0, 0%, 100%);
     font-weight: 600;
+    transition: .3s;
+  }
+
+  h2 a:hover{
+    color: hsl(178, 100%, 50%);
   }
 
   p{
@@ -59,6 +124,7 @@
 
   #sub-info{
     display: flex;
+    align-items: center;
     justify-content: space-between;
     margin-top: 0;
     padding-bottom: 20px;
@@ -68,5 +134,47 @@
   span{
     color: hsl(178, 100%, 50%);
   }
+
+  #sub-info span{
+    display: flex;
+    gap: 5px;
+  }
+
+  #sub-info #icon-eth{
+    width: 10px;
+    height: 20px;
+  }
+
+  #sub-info #icon-clock{
+    width: 20px;
+    height: 20px;
+  }
+
+  #created-by {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    padding: 20px 0;
+  }
   
+  #created-by img{
+    width: 30px;
+    height: 30px;
+    border: 1px solid #fff;
+    border-radius: 100%;
+  }
+
+  #created-by span{
+    color: hsl(215, 51%, 70%);
+  }
+
+  #created-by span a{
+    color: hsl(0, 0%, 100%);
+    text-decoration: none;
+    transition: .3s;
+  }
+
+  #created-by span a:hover{
+    color: hsl(178, 100%, 50%);
+  }
 </style>
